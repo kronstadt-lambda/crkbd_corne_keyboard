de49ddc56b087a91ae8cdb32e4d3e58f1d445c75
kronstadt-lambda <kronstadt.lambda@gmail.com>
2025-09-26 03:43:35 -0500

    spain tilde
    se agrego la letra ñ como tecla, entre otros cambios y mejoras

--- kronstadt-lambda/keymap.c
+++ kronstadt-lambda/keymap.c
@@ -18,6 +18,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+  OS_WIN = SAFE_RANGE,  // tecla para fijar "modo Windows"
+  OS_LIN,               // tecla para fijar "modo Linux"
+  ENE,                  // ñ
+  ENE_UP                // Ñ
+};
+
+static bool os_is_win = true;  // modo por defecto
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
@@ -27,7 +36,7 @@ LSFT_T(KC_ESC),   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                     
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL, KC_QUOT,    KC_Q,    KC_J,    KC_K,    KC_X,                         KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,LT(5,KC_SLSH),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                        KC_LGUI, MO(1), LT(5,KC_SPC), LT(3,KC_SPC), LT(2,KC_ENT), KC_RALT
+                                    LT(5,KC_LGUI),   MO(1),  KC_SPC,    LT(3,KC_SPC), LT(2,KC_ENT), KC_RALT
                                       //`--------------------------'  `--------------------------'
 
   ),
@@ -50,7 +59,7 @@ LSFT_T(KC_ESC),   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                     
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, KC_ASTR,    KC_4,    KC_5,    KC_6, KC_PLUS,                     RALT(KC_R), RALT(KC_4),RALT(KC_5),RALT(KC_6),RALT(KC_B), XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX,    KC_7,    KC_8,    KC_9,  KC_EQL,                     RALT(KC_N), RALT(KC_7),RALT(KC_8),RALT(KC_9), XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX,    KC_7,    KC_8,    KC_9,  KC_EQL,                     RALT(KC_N), RALT(KC_7),RALT(KC_8),RALT(KC_9),RALT(KC_0), XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                              KC_0,   MO(4),  KC_SPC,     KC_ENT, _______, KC_RALT
                                       //`--------------------------'  `--------------------------'
@@ -62,7 +71,7 @@ LSFT_T(KC_ESC),   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                     
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, XXXXXXX,  KC_EQL, KC_LCBR, KC_RCBR, XXXXXXX,                      KC_EXLM, KC_HASH, KC_SLSH, KC_BSLS, KC_PIPE, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX,                      XXXXXXX,  KC_GRV, KC_CIRC,   KC_AT, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX,                      XXXXXXX,  KC_GRV, KC_CIRC,   KC_AT,    ENE ,  ENE_UP,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, KC_RALT
                                       //`--------------------------'  `--------------------------'
@@ -72,9 +81,9 @@ LSFT_T(KC_ESC),   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                     
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, OS_LIN , XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, OS_WIN , XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, KC_RALT
                                       //`--------------------------'  `--------------------------'
@@ -136,4 +145,34 @@ void oled_render_layer_state(void) {
             oled_write_P(PSTR("Default"), false);
             break;
     }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (!record->event.pressed) return true;
+
+  switch (keycode) {
+    case OS_WIN:
+      os_is_win = true;
+      return false;
+    case OS_LIN:
+      os_is_win = false;
+      return false;
+
+    case ENE:     // ñ
+      if (os_is_win) {
+        SEND_STRING(SS_LALT("164"));                // Alt+164 → ñ (teclado numérico)
+      } else {
+        SEND_STRING(SS_LCTL(SS_LSFT("u")) "00f1" SS_TAP(X_SPACE)); // Ctrl+Shift+U 00f1 (Linux)
+      }
+      return false;
+
+    case ENE_UP:  // Ñ
+      if (os_is_win) {
+        SEND_STRING(SS_LALT("165"));                // Alt+165 → Ñ
+      } else {
+        SEND_STRING(SS_LCTL(SS_LSFT("u")) "00d1" SS_TAP(X_SPACE)); // Ctrl+Shift+U 00d1
+      }
+      return false;
+  }
+  return true;
 }
